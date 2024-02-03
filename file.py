442642c1212c87d86233758760e2bf9e9677961a
Lui Crowie <lui.crowie@crowzfx.co.uk>
2024-02-03 15:57:38 +0000

    added file utils and solver placeholder, also added documentation to all functions and copyright headers

---
+++ file.py
@@ -0,0 +1,62 @@
+"""
+    Copyright (C) 2024  Lui Crowie (@crowz-fx)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+class FileUtils:
+    """
+    Collection of file utility functions
+    """
+
+    def __init__(self) -> None:
+        """"""
+        pass
+
+    def write_file(self, file_name, file_contents):
+        """Takes contents input and writes out to specified file
+
+        Parameters
+        ----------
+        file_name : str
+          Relative path and name of file to write to
+        file_contents : str
+          Contents, already formatted to write to file
+
+        """
+        # TODO - implement
+        pass
+
+    def read_file(self, file_name):
+        """Reads the contents of file_name and returns
+
+        Parameters
+        ----------
+        file_name : str
+          Relative path and name of file to read
+
+        Returns
+        -------
+        str
+          Contents of file
+
+        Raises
+        ------
+        FileNotFound
+          If file cant not be found or opened
+
+        """
+        # TODO - implement
+        pass
--- gui.py
+++ gui.py
@@ -1,5 +1,21 @@
-import sys
+"""
+    Copyright (C) 2024  Lui Crowie (@crowz-fx)
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import sys
 from functools import partial
 
 from PySide6 import QtWidgets
@@ -9,7 +25,24 @@ from PySide6.QtCore import QFile, QIODevice, QSize
 
 
 class Gui:
+    """
+    All wrapper and GUI related functionality
+    """
+
     def __init__(self, ui_file_name="qt_gui.ui", args=[]):
+        """ "
+        Parameters
+        ----------
+        ui_file_name : str, default='qt_gui.ui'
+            Name of the Qt ui file to load and execute
+        args: list, default=[]
+            Arguments to pass into the Qt application object
+
+        Raises
+        ------
+        RuntimeException
+            If the `ui_file_name` does not exist or cannot be opened
+        """
         self.ui_file_name = ui_file_name
         self.ui_file = QFile(self.ui_file_name)
         self.app = None
@@ -22,9 +55,30 @@ class Gui:
             )
 
     def get_all_widgets(self):
+        """Return the list of all widgets within the QApplication
+
+        Return
+        ------
+        list[QWidget]
+            List of all the QWidget's within the application
+        """
         return self.app.allWidgets()
 
     def get_all_widgets_by_type(self, widget_type):
+        """Get all QWidgets of a certain QtWidget type in the application
+
+        Processes via lambda and filters to return only `widget_type` instance type widgets
+
+        Paramters
+        ---------
+        widget_type : QtWidget
+            Type of widget to filter
+
+        Return
+        ------
+        list[QWidget]
+            Widgets that matched the `isinstance()` criteria
+        """
         return list(
             filter(
                 lambda widget: isinstance(widget, widget_type), self.get_all_widgets()
@@ -32,6 +86,21 @@ class Gui:
         )
 
     def get_widget_by_type_and_name(self, name, widget_type):
+        """Get all QWidgets of a certain QtWidget type with a certain name in the application
+
+        Processes via lambda and filters to return only one `widget_type` instance type widgets
+        that matches the `objectName()` on the widget
+
+        Paramters
+        ---------
+        widget_type : QtWidget
+            Type of widget to filter
+
+        Return
+        ------
+        QWidget
+            Widgets that matched the `isinstance()` and name criteria
+        """
         return (
             list(
                 filter(
@@ -43,6 +112,21 @@ class Gui:
         )
 
     def get_all_widgets_by_name_starting_with(self, name):
+        """Get all QWidgets with a name that starts with `name` in the application
+
+        Processes via lambda and filters to return only QWidgets that have an `objectName()`
+        that starts with `name`
+
+        Paramters
+        ---------
+        name : str
+            Starting string of object name in QWidget
+
+        Return
+        ------
+        list[QWidget]
+            Widgets that object name start with `name`
+        """
         return (
             list(
                 filter(
@@ -54,6 +138,24 @@ class Gui:
         )
 
     def get_all_widgets_by_type_and_name_starting_with(self, name, widget_type):
+        """Get all QWidgets with a name that starts with `name` and are of type `widget_type`
+        in the application
+
+        Processes via lambda and filters to return only QWidgets that have an `objectName()`
+        that starts with `name` and `isinstance()` of `widget_type`
+
+        Paramters
+        ---------
+        name : str
+            Starting string of object name in QWidget
+        widget_type: QWidget
+            QWidget type
+
+        Return
+        ------
+        list[QWidget]
+            Widgets that object name start with `name` and match `widget_type`
+        """
         return (
             list(
                 filter(
@@ -65,6 +167,19 @@ class Gui:
         )
 
     def load(self, window_size=(640, 470)):
+        """Method to load the UI file and process 'precursor' setup such as binding buttons
+
+        Parameters
+        ----------
+        window_size : tuple, default=(640, 470)
+            Size of the window to generate when calling `self.run()`. Would advise not changing
+            this, I didn't put much effort into anchors on the GUI design
+
+        Raises
+        ------
+        RuntimeError
+            If could not load UI file and/or instantiate the window
+        """
         self.window = self.loader.load(self.ui_file)
         self.window.setFixedSize(QSize(*window_size))
         self.ui_file.close()
@@ -90,6 +205,12 @@ class Gui:
         ).clicked.connect(self.handle_input_toggle)
 
     def handle_input_toggle(self):
+        """Bound to the input button, handles updates to GUI to switch between `INPUT`
+        and `GUIDED` mode
+
+        `INPUT` - allows user to input into the cells the puzzle to be solved
+        `GUIDED` - locks all cels and application will gen/input values to be solved
+        """
         print("InputButton - Clicked")
         button_value = self.get_widget_by_type_and_name(
             "inputButton", QtWidgets.QPushButton
@@ -117,28 +238,79 @@ class Gui:
             square.setReadOnly(read_only_value)
 
     def solve_board(self):
+        """Bound to the `solve` button and does what it says on the tin
+
+        Calls into the solver class and processes
+        """
         print("SolveButton - Clicked")
         # TODO - add
 
     def generate_new_board(self, game_ease=0.5):
+        """Bound to the `generate` button and does what it says on the tin
+
+        Will clear the cells, and generate a new puzzle with `game_ease` setting
+        the level of masking i.e. how difficult to make it for a human to solve
+
+        Parameters
+        ----------
+        game_ease : float
+            How much of the generated board to hide, i.e. difficulty
+
+        """
         print("NewButton - Clicked")
         # TODO - add
 
     def run(self):
+        """Brains of the operation, post load, this is executed to actually
+        excute the application and display the window to the user
+
+        Bound to `sys.exit()` on the QApplication execution signal
+        """
         self.window.show()
         sys.exit(self.app.exec())
 
     def set_board_value(self, row, column, value):
+        """For given `row`, `column` set it to the value `value`
+
+        Note - all zero-indexed i.e. 8 rows not 9
+
+        Parameters
+        ----------
+        row : int
+        column : int
+        value : int
+        """
         self.get_widget_by_type_and_name(
             f"txtRow{row}Col{column}", QtWidgets.QPlainTextEdit
         ).setPlainText(value)
 
     def get_board_value(self, row, column):
+        """For given `row` and `column` get it's value
+
+        Note - all zero-indexed i.e. 8 rows not 9
+
+        Parameters
+        ----------
+        row : int
+        column : int
+
+        Return
+        ------
+        str
+            Value of the cell, not always an int hence str
+        """
         return self.get_widget_by_type_and_name(
             f"txtRow{row}Col{column}", QtWidgets.QPlainTextEdit
         ).getPlainText()
 
     def set_board_values(self, value="0"):
+        """Utility function to set all of the cells to a given `value`
+
+        Parameters
+        ----------
+        value : str
+            What to set the cell value to
+        """
         board_squares = self.get_all_widgets_by_type_and_name_starting_with(
             name="txt", widget_type=QtWidgets.QPlainTextEdit
         )
@@ -147,4 +319,5 @@ class Gui:
             square.setPlainText(value)
 
     def clear_board(self):
+        """Bound to the `clear` button, will set all of the cells to be blank"""
         self.set_board_values("")
